e79c992d42c382d7affcb3aca023a63f7ae7d028
smc-dotexe <shanelgmcguire@gmail.com>
2021-12-02 21:02:28 -0700

    completed step 2

---
+++ VogCodeChallenge.API/Classes/Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Classes.Entities
+{
+    public class Department : BaseEntity<Guid>
+    {
+        public string Name;
+
+        // If EF Core is installed, this Data Annotation will insure a unique address as required in step 2
+        //[Index(nameof(Address), IsUnique = true]
+        public string Address;
+        public List<Employees> Employees { get; set; }
+    }
+}
---
+++ VogCodeChallenge.API/Classes/Entities/Employees.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Classes.Entities
+{
+    public class Employees : BaseEntity<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+        public string MailingAddress { get; set; }
+
+        // Relationship with Department Entity
+        public Guid DepartmentId { get; set; }
+        public Department Department { get; set; }
+    }
+}
