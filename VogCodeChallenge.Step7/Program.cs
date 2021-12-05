ee9dc95a991c966e2b68410231fcbb3cf7b61bbc
smc-dotexe <shanelgmcguire@gmail.com>
2021-12-05 13:26:00 -0700

    Added new project for step_7 with QuestionClass and solution

---
+++ VogCodeChallenge.Step7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VogCodeChallenge.Step7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Step 7");
+            string names = QuestionClass.NameIterator();
+            Console.WriteLine(names);
+            Console.Write("==================================");
+        }
+    }
+}
---
+++ VogCodeChallenge.Step7/QuestionClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VogCodeChallenge.Step7
+{
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+
+        public static string NameIterator()
+        {
+            string names = string.Join(", ", NamesList);
+            return names;
+        }
+    }
+}
